d73f598a3bf219a33ae7bb125568528ff7a71b54
Soham Gupta <gsoham562@gmail.com>
2021-05-17 17:57:16 +0530

    add and update done

--- controller/router.js
+++ controller/router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const schema = require('./../model/schema');
 const validater = require('./validate');
+const uid = require('uniqid');
 
 
 
@@ -58,15 +59,163 @@ router.get('/classroom/:idType/:id/:classId/:where', (req, res) => {})
 
 
 
-router.get('/add/:what/:idType/:id', (req, res) => {})
+router.get('/add/:what/:idType/:id', (req, res) => {
+    if(req.params.idType === 'student'){
+        res.redirect('/error');
+    }
+    else if(req.params.idType === 'teacher'){
+        if(validater.valId(req.params.idType, req.params.id)){
+            if(req.params.what === 'class'){
+                res.render('addForm', {type : techerClass});
+            }else if(req.params.what === 'student'){
+                res.render('addForm', {type : student});
+            }else if(req.params.what === 'teacher'){
+                if(validater.valTeacherAdmin(req.params.id)){
+                    res.render('addForm', {type : teacher});
+                }
+                else{
+                    res.redirect('/error');
+                }
+            }else{
+                res.redirect('/error');
+            }
+        }
+    }else{
+        res.redirect('/error');
+    }
+})
 
-router.post('/add/:what/:idType/:id', (req, res) => {})
+router.post('/add/:what/:idType/:id', (req, res) => {
+    if(req.params.idType === 'student'){
+        res.redirect('/error');
+    }
+    else if(req.params.idType === 'teacher'){
+        if(validater.valId(req.params.idType, req.params.id)){
+            if(req.params.what === 'class'){
+                var data = new schema.techerClass({
+                    c_Id : uid(),
+                    Title : req.body.title,
+                    titleImg : Math.floor(Math.random() * 9) + 1, 
+                    createdOn : Date.now(),
+                    createdBy : req.params.id,
+                    Teachers : [],
+                    Students : []
+                })
+                data.save().then(res.redirect(`/classes/${req.params.idType}/${req.params.id}`)).catch(err => {res.redirect('/error')});
+            }else if(req.params.what === 'student'){
+                var data = new schema.student({
+                    s_Id : uid(),
+                    Name : req.body.name,
+                    Email : req.body.email,
+                    Username : req.body.username,
+                    Password : req.body.password  
+                })
+                data.save().then(res.redirect(`/classes/${req.params.idType}/${req.params.id}`)).catch(err => {res.redirect('/error')});
+            }else if(req.params.what === 'teacher'){
+                if(validater.valTeacherAdmin(req.params.id)){
+                    var data = new schema.teacher({
+                        t_Id : uid(),
+                        Name : req.body.name,
+                        Email : req.body.email,
+                        Username : req.body.username,
+                        Password: req.body.password,
+                        Admin : req.body.admin
+                    })
+                    data.save().then(res.redirect(`/classes/${req.params.idType}/${req.params.id}`)).catch(err => {res.redirect('/error')});
+                }
+                else{
+                    res.redirect('/error');
+                }
+            }else{
+                res.redirect('/error');
+            }
+        }
+    }else{
+        res.redirect('/error');
+    }
+})
 
 
 
-router.get('/update/:what/:idType/:id/:whatId', (req, res) => {})
+router.get('/update/:what/:idType/:id/:whatId', (req, res) => {
+    if(req.params.idType === 'student'){
+        res.redirect('/error');
+    }
+    else if(req.params.idType === 'teacher'){
+        if(validater.valId(req.params.idType, req.params.id)){
+            if(req.params.what === 'class'){
+                var data = validater.validateAndFind('class', req.params.whatId);
+                if(data == 'error'){
+                    res.redirect('/error');
+                }else{
+                    res.render('updateForm', {type : 'techerClass', dataShow : data});
+                }
+            }else if(req.params.what === 'student'){
+                var data = validater.validateAndFind('student', req.params.whatId);
+                if(data == 'error'){
+                    res.redirect('/error');
+                }else{
+                    res.render('updateForm', {type : 'student', dataShow : data});
+                }
+            }else if(req.params.what === 'teacher'){
+                if(validater.valTeacherAdmin(req.params.id)){
+                    var data = validater.validateAndFind('student', req.params.whatId);
+                    if(data == 'error'){
+                        res.redirect('/error');
+                    }else{
+                        res.render('updateForm', {type : 'teacher', dataShow : data});
+                    }
+                }
+                else{
+                    res.redirect('/error');
+                }
+            }else{
+                res.redirect('/error');
+            }
+        }
+    }else{
+        res.redirect('/error');
+    }
+})
 
-router.post('/update/:what/:idType/:id/:whatId', (req, res) => {})
+router.post('/update/:what/:idType/:id/:whatId', (req, res) => {
+    if(req.params.idType === 'student'){
+        res.redirect('/error');
+    }
+    else if(req.params.idType === 'teacher'){
+        if(validater.valId(req.params.idType, req.params.id)){
+            if(req.params.what === 'class'){
+                schema.techerClass.findOneAndUpdate({c_Id : req.params.whatId},{
+                    Title : req.body.title
+                },null, function(err){if(err) return handleError(err);})
+            }else if(req.params.what === 'student'){
+                schema.student.findOneAndUpdate({s_Id : req.params.whatId},{
+                    Name : req.body.name,
+                    Email : req.body.email,
+                    Username : req.body.username,
+                    Password : req.body.password  
+                },null, function(err){if(err) return handleError(err);});
+``            }else if(req.params.what === 'teacher'){
+                if(validater.valTeacherAdmin(req.params.id)){
+                    schema.teacher.findOneAndUpdate({t_Id : req.params.whatId},{
+                        Name : req.body.name,
+                        Email : req.body.email,
+                        Username : req.body.username,
+                        Password: req.body.password,
+                        Admin : req.body.admin
+                    },null, function(err){if(err) return handleError(err);})
+                }
+                else{
+                    res.redirect('/error');
+                }
+            }else{
+                res.redirect('/error');
+            }
+        }
+    }else{
+        res.redirect('/error');
+    }
+})
 
 
 
--- controller/validate.js
+++ controller/validate.js
@@ -22,4 +22,41 @@ function valId(idType, id){
     }
 }
 
-module.exports = {valId};
+function valTeacherAdmin(id){
+    schema.teacher.findOne({t_Id : id}, 'admin' ,function(err, result){
+        if(err) return handleError(err);
+        return result.admin;
+    })
+}
+
+function validateAndFind(what, id){
+    if(what === 'class'){
+        schema.techerClass.findOne({c_Id : id}, 'c_Id Title', function(err, data){
+            if(err) return handleError(err);
+            if(data !== null){
+                return data
+            }else{
+                return 'error'
+            }
+        })
+    }else if(what === 'student'){
+        schema.techerClass.findOne({s_Id : id}, 's_Id Name Email Username Password', function(err, data){
+            if(err) return handleError(err);
+            if(data !== null){
+                return data
+            }else{
+                return 'error'
+            }
+        })
+    }else if(what === 'teacher'){
+        schema.techerClass.findOne({t_Id : id}, 't_Id Name Email Username Password Admin', function(err, data){
+            if(err) return handleError(err);
+            if(data !== null){
+                return data
+            }else{
+                return 'error'
+            }
+        })
+    }
+}
+module.exports = {valId, valTeacherAdmin, validateAndFind};
